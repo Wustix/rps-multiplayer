9e830bb0c55a82cd01af1658469d0ae9effa39c4
unknown <wroehrman@yahoo.com>
2018-06-23 00:18:43 -0500

    updates to rps files

---
+++ assets/javascript/rpsgame.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var config = {
+        apiKey: "AIzaSyCwRSRHRE3vcOcjvYZ6H4JRHz8wiQyGfSo",
+        authDomain: "rps-multiplayer-213d5.firebaseapp.com",
+        databaseURL: "https://rps-multiplayer-213d5.firebaseio.com",
+        projectId: "rps-multiplayer-213d5",
+        storageBucket: "",
+        messagingSenderId: "776452580515"
+    };
+    firebase.initializeApp(config);
+
+});
