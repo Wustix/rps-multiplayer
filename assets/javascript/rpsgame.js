dd673560152cafed3972d36b479507a9d3a8ed75
unknown <wroehrman@yahoo.com>
2018-06-24 00:47:51 -0500

    updates to rps files

--- assets/javascript/rpsgame.js
+++ assets/javascript/rpsgame.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    $("#start-btn").on("click", function (event) {
+        event.preventDefault();
+        // Capture User Input and store them into variable
+        var name = $("#input-name").val().trim();
+        var p = $("<p>Hi " + name + "You are Player 1</p>");
+        
+        // Console log each of the user inputs to confirm we are receiving them
+        console.log(name)
+        //Output user inout into correct html area replacing text area and button
+        $("#name-added-1").text(p);
+        console.log(p)
+        $("#input-name").val("");
+        $("#input-name").hide();
+        $("#start-btn").hide();
+        
+
+    });
 
 
 
@@ -21,14 +38,15 @@ $(document).ready(function () {
 
 
 
-    var config = {
-        apiKey: "AIzaSyCwRSRHRE3vcOcjvYZ6H4JRHz8wiQyGfSo",
-        authDomain: "rps-multiplayer-213d5.firebaseapp.com",
-        databaseURL: "https://rps-multiplayer-213d5.firebaseio.com",
-        projectId: "rps-multiplayer-213d5",
-        storageBucket: "",
-        messagingSenderId: "776452580515"
-    };
-    firebase.initializeApp(config);
+
+    // var config = {
+    //     apiKey: "AIzaSyCwRSRHRE3vcOcjvYZ6H4JRHz8wiQyGfSo",
+    //     authDomain: "rps-multiplayer-213d5.firebaseapp.com",
+    //     databaseURL: "https://rps-multiplayer-213d5.firebaseio.com",
+    //     projectId: "rps-multiplayer-213d5",
+    //     storageBucket: "",
+    //     messagingSenderId: "776452580515"
+    // };
+    // firebase.initializeApp(config);
 
 });
