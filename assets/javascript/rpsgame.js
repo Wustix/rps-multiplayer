9655246378f74a567103be310f7a9d37fa51bdf6
unknown <wroehrman@yahoo.com>
2018-06-24 21:38:07 -0500

    files update

--- assets/javascript/rpsgame.js
+++ assets/javascript/rpsgame.js
@@ -1,22 +1,59 @@
 $(document).ready(function () {
 
+    var rock = $("img").attr({ src: "./assets/images/Rock.jpg", height: "140px", width: "175px" });
+    var paper = $("img").attr({ src: "./assets/images/Paper.jpg", height: "140px", width: "175px" });
+    var scissors = $("img").attr({ src: "./assets/images/Scissors.jpg", height: "140px", width: "175px" });
+    var playerTurn = $("<p>It's Your Turn!</p>");
+    var wins = 0;
+    var losses = 0;
+
+    
+
+
     $("#start-btn").on("click", function (event) {
         event.preventDefault();
         // Capture User Input and store them into variable
         var name = $("#input-name").val().trim();
-        var p = $("<p>Hi " + name + "You are Player 1</p>");
-        
+        var p = $("<p>Hi " + name + " You are Player 1</p>");
+
         // Console log each of the user inputs to confirm we are receiving them
         console.log(name)
-        //Output user inout into correct html area replacing text area and button
-        $("#name-added-1").text(p);
+        //Output user input into correct html area replacing text area and button
+        $("#name-added-1").html(p);
+        $("#player1-box").text(name);
+        $("#player-turn").html(playerTurn)
         console.log(p)
         $("#input-name").val("");
         $("#input-name").hide();
         $("#start-btn").hide();
-        
 
     });
+
+    
+
+    $(document).on("click", function () {
+
+        var userGuess = [rock, paper, scissors];
+
+        if (userGuess === rock) {
+            $("#player1-box").hide(paper);
+            $("#player1-box").hide(scissors);
+        }
+        if (userGuess === paper) {
+            $("#player1-box").hide(rock);
+            $("#player1-box").hide(scissors);
+        }
+        if (userGuess === scissors) {
+            $("#player1-box").hide(rock);
+            $("#player1-box").hide(paper);
+        }
+
+
+
+
+    });
+
+
 
 
 
